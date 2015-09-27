0a867ba53c1e6315e24356d3bd2bf8d42575fa4c
Michael Pritchin <pritchinmihail@gmail.com>
2015-09-28 00:33:49 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+  var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+
+function compile(watch) {
+  var bundler = watchify(browserify('./src/app/app.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('http-server', function() {
+  connect()
+    .use(connect.static('./src/www/index.html'))
+    .listen('8000');
+});
+
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
